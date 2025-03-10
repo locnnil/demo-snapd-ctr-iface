03fcce3ca86fa4cd2784943cbb6f9357d46f2eb0
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-03-10 20:16:05 -0300

    drop unrelated comments
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- main.go
+++ main.go
@@ -9,13 +9,8 @@ import (
 )
 
 var clientConfig = client.Config{
-	// snapctl should not try to read $HOME/.snap/auth.json, this will
-	// result in apparmor denials and configure task failures
-	// (LP: #1660941)
 	DisableAuth: true,
-
-	// we need the less privileged snap socket in snapctl
-	Socket: dirs.SnapSocket,
+	Socket:      dirs.SnapSocket,
 }
 
 func main() {
